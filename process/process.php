87a9697e40afe21d04170d021cf9844c45d723cf
ThanhNhan1998 <moitapchojgame@gmail.com>
2020-05-18 21:53:40 +0700

    sucomoi

--- process/process.php
+++ process/process.php
@@ -1,4 +1,4 @@
 <?php 
-    $conn = new mysqli("localhost", "root", "", "helpdesk_system") or die("Kết nối thất bại");
+    $conn = new mysqli("localhost", "root", "boyanhNHAN@@1998", "helpdesk_system") or die("Kết nối thất bại");
     mysqli_query($conn, "SET NAMES UTF8");
 ?>
